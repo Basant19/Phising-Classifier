8872f79345e6c89978d1e73e9787e6b39e7b1cb5
Basant <btirkey1208@gmail.com>
2024-09-29 17:26:34 +0530

    new changes

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
-import logging
-import os
-from datetime import datetime
+from flask import Flask, render_template, jsonify, request, send_file
+from src.exception import CustomException
+from src.logger import logging as lg
+import os,sys
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+from src.pipeline.train_pipeline import TrainingPipeline
+from src.pipeline.predict_pipeline import PredictionPipeline
 
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+app = Flask(__name__)
 
-os.makedirs(logs_path, exist_ok=True)
+@app.route("/")
+def home():
+    return jsonify("home")
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+@app.route("/train")
+def train_route():
+    try:
+        train_pipeline = TrainingPipeline()
+        train_pipeline.run_pipeline()
+
+        return "Training Completed."
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    
+    try:
+        if request.method == 'POST':
+            prediction_pipeline = PredictionPipeline(request)
+            prediction_file_detail = prediction_pipeline.run_pipeline()
+
+            lg.info("prediction completed. Downloading prediction file.")
+            return send_file(prediction_file_detail.prediction_file_path,
+                            download_name= prediction_file_detail.prediction_file_name,
+                            as_attachment= True)
+        
+        else:
+            return render_template('prediction.html')
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug= True)
